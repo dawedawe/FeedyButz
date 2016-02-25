456e187ae647c114e2b1355adc4cbf73ea3e14e7
David Schaefer <david.schaefer1@smail.inf.h-brs.de>
2016-02-26 00:05:05 +0100

    initial import

---
+++ FeedReader/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Windows.Data.Xml;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using FeedReader.Model;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FeedReader
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FeedItems = new ObservableCollection<FeedItem>();
+        }
+
+        private ObservableCollection<FeedItem> FeedItems;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshFeeds();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshFeeds();
+        }
+
+        private void RefreshFeeds()
+        {
+            List<string> feeds = new List<string> {
+                "http://heise.de.feedsportal.com/c/35207/f/653902/index.rss",
+                "http://golem.de.dynamic.feedsportal.com/pf/578068/http://rss.golem.de/rss.php?feed=RSS1.0",
+                "http://www.faz.net/rss/aktuell/",
+            };
+
+            FeedItems.Clear();
+            foreach (string feed in feeds)
+            {
+                Model.FeedItemManager.GetFeedUrls(feed, FeedItems);
+            }
+        }
+    }
+}
---
+++ FeedReader/Model/FeedItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedReader.Model
+{
+    public class FeedItem
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class FeedItemManager
+    {
+        public static async void GetFeedUrls(string feedUrl, ObservableCollection<FeedItem> feedItems)
+        {
+            Windows.Data.Xml.Dom.XmlDocument xmlDoc = new Windows.Data.Xml.Dom.XmlDocument();
+            HttpWebRequest request = HttpWebRequest.CreateHttp(feedUrl);
+
+            WebResponse response = await request.GetResponseAsync();
+            Stream s = response.GetResponseStream();
+            StreamReader r = new StreamReader(s);
+            xmlDoc.LoadXml(r.ReadToEnd());
+
+            Windows.Data.Xml.Dom.XmlNodeList rssNodes = xmlDoc.SelectNodes("rss/channel/item");
+
+            // Iterate through the items in the RSS file
+            foreach (Windows.Data.Xml.Dom.IXmlNode rssNode in rssNodes)
+            {
+                Windows.Data.Xml.Dom.IXmlNode rssSubNode = rssNode.SelectSingleNode("title");
+                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("link");
+                string link = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("description");
+                string description = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                feedItems.Add(new FeedItem() { Title = title, Url = link });
+            }
+        }
+    }
+}
